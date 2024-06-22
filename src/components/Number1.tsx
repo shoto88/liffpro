404cf1eceff5e45c66238eefab4aa780f874b5ac
Otone Hamada <otonehamada@OtonenoMacBook-Pro.local>
2024-06-22 11:29:03 +0900

    first commit

--- src/components/Number1.tsx
+++ src/components/Number1.tsx
@@ -174,7 +174,7 @@ function Number1() {
           ) : ticketError ? (
             <p className="text-red-500 text-sm">再ログインが必要です</p>
           ) : ticketData?.ticket_number ? (
-            <span className="text-4xl flex items-center justify-center  mt-6 font-semibold">{ticketData.ticket_number}</span>
+            <span className="text-4xl flex items-center justify-center  mt-5 font-semibold">{ticketData.ticket_number}</span>
           ) : (
             <p className="text-gray-500">LINEから発券されていません</p>
           )}
