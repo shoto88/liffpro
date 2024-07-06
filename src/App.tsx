732f9aa505bfb6b5650ce593a30a61eb7c8af68d
shoto88 <shotoharu@icloud.com>
2024-07-07 08:56:21 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Number1 from "./components/Number1.tsx";
 import Form1 from "./components/Form";
 import "../app/globals.css"
 import { Route, Routes } from 'react-router-dom';
+import WaitingTimeChecker from "./components/Waiting.tsx";
 const App = () => {
   return (
   <div>
@@ -11,6 +12,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Number1 />} />
       <Route path="/form" element={<Form1 />} />
+      <Route path="/waiting" element={<WaitingTimeChecker />} />
     </Routes>
   </div>
   </div>
