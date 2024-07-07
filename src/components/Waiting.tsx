1b82df5a53d484d234ea661ec1d1365644a8fab2
shoto88 <shotoharu@icloud.com>
2024-07-07 09:56:32 +0900

    test

--- src/components/Waiting.tsx
+++ src/components/Waiting.tsx
@@ -180,7 +180,7 @@ function WaitingTimeChecker() {
                   <div className="mb-4 p-2 bg-blue-100 rounded-md">
     <p className="text-sm text-left text-blue-800 mb-2">
       発券番号は以下です。<br />
-      来院時、受付にて番号に変更があった場合は新しい番号を入力してください。
+      受付にて番号に変更があった場合は新しい番号を入力してください。
     </p>
                   </div>
                 ) : (
